b15f3b7fee638043257339470eb1beec68bd07f1
Mateusz Martin <mateusz.martinn@gmail.com>
2023-02-19 22:56:41 +0100

    <Statistics> functional comp refactor

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,35 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import { StyledStatistics } from './Statistics.styled';
 
-export class Statistics extends Component {
-  render() {
-    const { state, total, positivePercentage } = this.props;
+export const Statistics = props => {
+  const { state, total, positivePercentage } = props;
 
-    return (
-      <StyledStatistics>
-        <ul>
-          <li>
-            Good: <span>{state.good}</span>{' '}
-          </li>
-          <li>
-            Neutral: <span>{state.neutral}</span>{' '}
-          </li>
-          <li>
-            Bad: <span>{state.bad}</span>{' '}
-          </li>
-          <li>
-            Total: <span>{total}</span>{' '}
-          </li>
-          <li>
-            Positive Feedback: <span id='positivePercentage'>{positivePercentage}%</span>
-          </li>
-        </ul>
-      </StyledStatistics>
-    );
-  }
-}
+  return (
+    <StyledStatistics>
+      <ul>
+        <li>
+          Good: <span>{state[0]}</span>{' '}
+        </li>
+        <li>
+          Neutral: <span>{state[1]}</span>{' '}
+        </li>
+        <li>
+          Bad: <span>{state[2]}</span>{' '}
+        </li>
+        <li>
+          Total: <span>{total}</span>{' '}
+        </li>
+        <li>
+          Positive Feedback:{' '}
+          <span id="positivePercentage">{positivePercentage}%</span>
+        </li>
+      </ul>
+    </StyledStatistics>
+  );
+};
 Statistics.propTypes = {
   state: PropTypes.object,
   total: PropTypes.any,
